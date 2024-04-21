334055f7b0d7145309a8257bab22fc508efcdda6
Hailemeskel Getaneh <159171440+Hailemeskel-Getaneh@users.noreply.github.com>
2024-04-21 04:22:18 -0700

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+// ============ typing Animation ===========
+var typed = new Typed(".typing", {
+  strings: [" ", "Student", "Erudition Seeker", "Mighty Worker"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
